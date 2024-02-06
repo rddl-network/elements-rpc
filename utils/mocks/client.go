243f412f8b63945fba3465ab760fb77b7e342448
Lorenz Herzberger <lorenzherzberger@gmail.com>
2024-02-06 11:55:06 +0100

    feat: add amount to mock result
    Signed-off-by: Lorenz Herzberger <lorenzherzberger@gmail.com>

--- utils/mocks/client.go
+++ utils/mocks/client.go
@@ -33,8 +33,10 @@ var (
 	signRawTransactionWithWalletResult = types.SignRawTransactionWithWalletResult{Hex: zeros, Complete: true}
 	testMempoolAcceptResult            = types.TestMempoolAcceptResult{Allowed: true, Txid: zeros, Vsize: 0, Wtxid: ""}
 	testMempoolAcceptResults           = []types.TestMempoolAcceptResult{testMempoolAcceptResult}
-	transactionResult                  = types.GetTransactionResult{Hex: zeros, TxID: zeros}
-	zeros                              = "0000000000000000000000000000000000000000000000000000000000000000"
+	transactionResult                  = types.GetTransactionResult{Hex: zeros, TxID: zeros, Amount: map[string]float64{
+		"7add40beb27df701e02ee85089c5bc0021bc813823fedb5f1dcb5debda7f3da9": 10000,
+	}}
+	zeros = "0000000000000000000000000000000000000000000000000000000000000000"
 )
 
 // GetDoFunc fetches the mock client's `Do` func
