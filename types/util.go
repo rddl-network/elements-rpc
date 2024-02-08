c4ecb3ee40767660505716906c889a37a8b81b0b
Lorenz Herzberger <64837895+LaurentMontBlanc@users.noreply.github.com>
2024-02-08 14:19:51 +0100

    feat: add deriveddresses call (#11)
    * feat: add testDeriveAddressResult to mock
    
    Signed-off-by: Lorenz Herzberger <lorenzherzberger@gmail.com>

---
+++ types/util.go
@@ -0,0 +1,4 @@
+package types
+
+// DeriveAddressesResult models the data from the deriveaddresses command.
+type DeriveAddressesResult []string
--- utils/mocks/client.go
+++ utils/mocks/client.go
@@ -36,7 +36,8 @@ var (
 	transactionResult                  = types.GetTransactionResult{Hex: zeros, TxID: zeros, Amount: map[string]float64{
 		"7add40beb27df701e02ee85089c5bc0021bc813823fedb5f1dcb5debda7f3da9": 10000,
 	}}
-	zeros = "0000000000000000000000000000000000000000000000000000000000000000"
+	testDeriveAddressesResult = types.DeriveAddressesResult{address}
+	zeros                     = "0000000000000000000000000000000000000000000000000000000000000000"
 )
 
 // GetDoFunc fetches the mock client's `Do` func
@@ -80,6 +81,8 @@ func GetDoFunc(req *http.Request) (*http.Response, error) {
 		response.Result = signRawTransactionWithWalletResult
 	case types.MethodTestMempoolAccept:
 		response.Result = testMempoolAcceptResults
+	case types.MethodDeriveAddresses:
+		response.Result = testDeriveAddressesResult
 	default:
 		response.Result = nil
 		response.Error.Code = -1337
